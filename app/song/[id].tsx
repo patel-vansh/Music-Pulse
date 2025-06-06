43192fcbf084bae3cd71718d8c443f0f189c37ab
patel-vansh <developer.patelvansh@gmail.com>
2025-06-06 12:23:26 +0530

    Added basic routing and icons.

---
+++ app/song/[id].tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native'
+import React from 'react'
+
+const SongDetails = () => {
+    return (
+        <View>
+            <Text>SongDetails</Text>
+        </View>
+    )
+}
+
+export default SongDetails
---
+++ constants/icons.ts
@@ -0,0 +1,11 @@
+import home from '@/assets/icons/home.png';
+import search from '@/assets/icons/search.png';
+import saved from '@/assets/icons/saved.png';
+import person from '@/assets/icons/person.png';
+
+export const icons = {
+    home,
+    search,
+    saved,
+    person,
+};
